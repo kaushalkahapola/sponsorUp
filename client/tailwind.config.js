ddd163440421fd3557d0918b4cdca3120f46b7f2
kaushalkahapola <kaushal.21@cse.mrt.ac.lk>
2024-06-19 11:17:25 -0700

    changed the structure

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,8 +12,5 @@ export default {
     },
   },
   plugins: [],
-  // paths: {
-  //   // fonts: './src/assets/fonts',
-  // },
 }
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import {router} from './routes/apiRoute.js';
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
