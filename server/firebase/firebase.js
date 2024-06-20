5ebe51e5f5e0c613f0121bcd00a9eb9ad3364f3d
kaushalkahapola <kaushal.21@cse.mrt.ac.lk>
2024-06-20 17:11:25 +0530

    Configure firebase for client and server

--- server/firebase/firebase.js
+++ server/firebase/firebase.js
@@ -1,24 +1,25 @@
-import firebase from 'firebase/compat/app';
-// import 'firebase/firestore';
-import 'firebase/compat/firestore';
+import admin from 'firebase-admin';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId:process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+  };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-// Initialize Firestore
-const db = firebase.firestore();
+const db = admin.firestore();
 
-export { db };
-
+export  { admin, db };
