6569506ee20b3614d7464ff4ab782288994dbb88
kaushalkahapola <kaushal.21@cse.mrt.ac.lk>
2024-06-20 20:12:21 +0530

    Added SponsorCard Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Test from "./pages/Test";
-import { Theme } from "@radix-ui/themes";
+import { Theme , ThemePanel} from "@radix-ui/themes";
 import '@radix-ui/themes/styles.css'
 
 function App() {
   return (
-    <Theme>
+    <Theme accentColor="purple" grayColor="gray">
       <Router>
         <Routes>
           <Route path="/test" element={<Test />} />
---
+++ client/src/components/SponsorCard.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Card, Flex, Box, Avatar, Badge } from "@radix-ui/themes";
+
+const SponsorCard = ({
+  name = "Company Name",
+  location = "Location",
+  status = "status",
+  description = "Description about the company",
+  categories = ["Music", "Education", "IT"],
+}) => {
+  return (
+    <div className="font-poppins text-gray">
+      <Box maxWidth="700px">
+        <Card className="p-5">
+          <Flex direction="column" align="start" justify="center" gap="5">
+            <Box>
+              <Flex gapX="4">
+                <Avatar
+                  src="/"
+                  fallback={"?"}
+                  size="6"
+                  radius="full"
+                  className="cursor-pointer"
+                />
+                <Box>
+                  <Flex
+                    className="h-full"
+                    direction="column"
+                    align="start"
+                    justify="center"
+                  >
+                    <Box className="text-black font-bold">{name}</Box>
+                    <Box>{location}</Box>
+                    <Badge mt="1" color="purple">
+                      {status}
+                    </Badge>
+                  </Flex>
+                </Box>
+              </Flex>
+            </Box>
+            <Flex direction="column" align="start" justify="center" gapY="4">
+              <Box>
+                <Flex
+                  className=" text-sm"
+                  direction="column"
+                  align="start"
+                  justify="center"
+                >
+                  <Box>{description}</Box>
+                </Flex>
+              </Box>
+              <Box className="text-sm">
+                <Box className="text-black font-bold mb-2">
+                  Interested Categories
+                </Box>
+                <Flex gap="2">
+                  {categories.map((category, index) => (
+                    <Badge key={index} color="blue">
+                      {category}
+                    </Badge>
+                  ))}
+                  {/* <Badge color="orange">Music</Badge>
+                  <Badge color="blue">Music</Badge>
+                  <Badge color="green">Music</Badge> */}
+                </Flex>
+              </Box>
+            </Flex>
+          </Flex>
+        </Card>
+      </Box>
+    </div>
+  );
+};
+
+export default SponsorCard;
