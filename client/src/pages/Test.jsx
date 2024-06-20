0d80989e2534b511d367f2d01efb6091dbad65f9
kaushalkahapola <kaushal.21@cse.mrt.ac.lk>
2024-06-20 19:06:56 +0530

    default test file

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import OrganizersNavbar from "../components/OrganizersNavbar";
 
-return (
-  <div>
-    <h1>Test Page</h1>
-    <p>Welcome to the Test page.</p>
-  </div>
-);
+import React from 'react';
+
+function Test() {
+
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <p>Welcome to the Test page.</p>
+    </div>
+  );
+}
 
 export default Test;
