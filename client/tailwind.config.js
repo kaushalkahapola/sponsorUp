340aa256f525a1f8f70c3527c108f9490aac0c40
kaushalkahapola <kaushal.21@cse.mrt.ac.lk>
2024-06-20 13:51:03 +0530

    Colors defined in tailwing config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,27 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#6D31EDFF",
+          100: "#F5F1FEFF",
+          150 : "#E4D9FCFF",
+          200: "#D3C1FA73",
+          500: "#6D31EDFF",
+        },
+        gray: {
+          DEFAULT: "#6c757d",
+          100: "#ced4da",
+          200: "#adb5bd",
+          300: "#6c757d",
+          400: "#495057",
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#1E1E2D",
+          200: "#232533",
+        },
+      },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
