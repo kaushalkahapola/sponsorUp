2f2f1f1da56e2aad0e5b37ace25d96fb18df7125
kaushalkahapola <kaushal.21@cse.mrt.ac.lk>
2024-06-20 19:01:29 +0530

    Organization Navbar.1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Test from './pages/Test';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Test from "./pages/Test";
+import { Theme } from "@radix-ui/themes";
+import '@radix-ui/themes/styles.css'
 
 function App() {
-
   return (
-    <Router>
-    <Routes>
-      <Route path="/test" element={<Test />} />
-    </Routes>
-    </Router>
+    <Theme>
+      <Router>
+        <Routes>
+          <Route path="/test" element={<Test />} />
+        </Routes>
+      </Router>
+    </Theme>
   );
 }
 
---
+++ client/src/components/OrganizersNavbar.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Link,
+  Text,
+} from "@radix-ui/themes";
+
+const OrganizersNavbar = () => {
+  const links = [
+    {
+      label: "Home",
+      path: "/",
+    },
+    {
+      label: "My Events",
+      path: "/",
+    },
+  ];
+
+  return (
+    <nav className="border-b mb-6 px-5 font-poppins">
+      <Container>
+        <Flex align="center" py="3" justify="between">
+          <Flex gap="5" align="center">
+            <Link href="/" className="text-gray-400 font-bold text-xl">Sponsor<span className="text-primary">Up</span> </Link>
+            <ul className="flex space-x-6 text-sm">
+              {links.map((link, index) => (
+                <li key={index}>
+                  <Link href={link.path} className="text-zinc-500">
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Flex>
+            <Box>
+              <Flex gap="3" align="center">
+                <DropdownMenu.Root>
+                  <DropdownMenu.Trigger>
+                    <Avatar
+                      src="/"
+                      fallback={"?"}
+                      size="2"
+                      radius="full"
+                      className="cursor-pointer"
+                    />
+                  </DropdownMenu.Trigger>
+                  <DropdownMenu.Content>
+                    <DropdownMenu.Label>
+                      <Text size="2" className="font-poppins">example@gmail.com</Text>
+                    </DropdownMenu.Label>
+                    <DropdownMenu.Item>
+                      <Text>SignOut</Text>
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Root>
+                <Flex
+                direction='column' justify='center' align='start'>
+                <Text
+                  size="2"
+                  weight="medium"
+                >{`John Smith`}</Text>
+                <Text size='1' weight='light'>test Orginaztion</Text>
+                </Flex>
+              </Flex>
+            </Box>
+          </Flex>
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+export default OrganizersNavbar;
