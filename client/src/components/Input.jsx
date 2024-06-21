080266973a588ea2f7ab74615d2f73ef264f1ad6
ChathumiDewmini <114095966+ChathumiDewmini@users.noreply.github.com>
2024-06-22 05:03:04 +0530

    Input.jsx

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -5,8 +5,8 @@ function Input() {
     const [username, setUsername] = useState('');
   
     return (
-      <div className="flex justify-center items-center h-screen bg-gray-100">
-        <div className="relative w-80 h-15 border-2 border-gray-300 rounded-md bg-white p-3">
+      <div className="px-4 py-4 ">
+        <div className="relative w-80 h-12 border-2 border-gray-300 rounded-md bg-white p-3">
           <div className="absolute left-3 top-1 text-xs text-black font-bold">
             Email
           </div>
@@ -14,7 +14,7 @@ function Input() {
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            className="w-full mt-5 border-none outline-none text-gray-700 text-sm placeholder-gray-400"
+            className="w-full mt-2 border-none outline-none text-gray-700 text-sm placeholder-gray-400"
             placeholder="example@gmail.com"
           />
         </div>
