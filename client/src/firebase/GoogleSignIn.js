8c9be536b6ed341beb3b573e9274ad1f3e585d9b
kaushalkahapola <kaushal.21@cse.mrt.ac.lk>
2024-06-29 14:42:01 +0530

    create logic to adding users to the database

--- client/src/firebase/GoogleSignIn.js
+++ client/src/firebase/GoogleSignIn.js
@@ -1,11 +1,21 @@
-import { auth, googleProvider } from './firebase';
+import { auth, googleProvider, db } from './firebase';
 import {signInWithPopup } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 
 // Google Sign In function
 const GoogleSignIn = async () => {
     try {
         const result = await signInWithPopup(auth, googleProvider);
         const user = result.user;
+        
+
+        const data = {
+            email: user.email,
+            uid: user.uid,
+        };
+
+        await setDoc(doc(db, "users", user.uid), data);
+
         console.log('User signed in with Google:', user);console.log(user);
     } catch (error) {
         console.error('Error signing in with Google:', error.message);
--- client/src/firebase/SignUp.js
+++ client/src/firebase/SignUp.js
@@ -1,10 +1,20 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from './firebase'; 
+import { auth, db } from './firebase';
+import { doc, setDoc } from "firebase/firestore";
+
 
 const signUpFn = async (email, password) => {
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
+    
+    const data = {
+      email: user.email,
+      uid: user.uid,
+    };
+    
+    await setDoc(doc(db, "users", user.uid ), data); 
+
     console.log('User signed up successfully:', user);
     return user;
   } catch (error) {
