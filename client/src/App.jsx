f23f706e6e7d2d5fc354ce7a4db7051549aedbcc
anush12345 <75567234+anush12345@users.noreply.github.com>
2024-07-11 21:54:18 +0530

    feat: Add "Obq" page to the application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,6 +18,7 @@ import SearchEventsPage from "./pages/SearchEventsPage";
 import Test from "./pages/Test";
 import MyEvents from "./pages/MyEvents";
 import SponsorDetailsPage from "./pages/SponsorDetailsPage";
+import Obq from "./pages/Obq";
 
 function App() {
   // just a simple useEffect to check if the user is signed in or not
@@ -61,6 +62,7 @@ function App() {
           <Route path="/test" element={<Test />} />
           <Route path="/account/myevents" element={<MyEvents />} />
           <Route path="/sponsor/:id" element={<SponsorDetailsPage />} />
+          <Route path="/obq" element={<Obq />} />
         </Routes>
       </Router>
       {/* <ThemePanel /> */}
