66cb9d1c90e51a9022067509b141752d2e1c9206
anush12345 <75567234+anush12345@users.noreply.github.com>
2024-07-12 19:58:56 +0530

    refactor: Update GoogleSignIn.js for better error handling and document existence check

--- client/src/firebase/GoogleSignIn.js
+++ client/src/firebase/GoogleSignIn.js
@@ -1,25 +1,32 @@
-import { auth, googleProvider, db } from './firebase';
-import {signInWithPopup } from "firebase/auth";
+import { auth, googleProvider, db } from "./firebase";
+import { signInWithPopup } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 // Google Sign In function
 const GoogleSignIn = async () => {
-    try {
-        const result = await signInWithPopup(auth, googleProvider);
-        const user = result.user;
-        
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    const user = result.user;
 
-        const data = {
-            email: user.email,
-            uid: user.uid,
-        };
+    const data = {
+      email: user.email,
+      uid: user.uid,
+    };
 
-        await setDoc(doc(db, "users", user.uid), data);
+    const userRef = doc(db, "users", user.uid);
+    const userDoc = await getDoc(userRef);
 
-        console.log('User signed in with Google:', user);console.log(user);
-    } catch (error) {
-        console.error('Error signing in with Google:', error.message);
+    // Check if the document exists
+    if (!userDoc.exists()) {
+      await setDoc(doc(db, "users", user.uid), data);
     }
+
+    console.log("User signed in with Google:", user);
+    console.log(user);
+  } catch (error) {
+    console.error("Error signing in with Google:", error.message);
+  }
 };
 
 export default GoogleSignIn;
