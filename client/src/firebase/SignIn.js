b408b589c487c4c346628815694c8d7b10d9e375
anush12345 <75567234+anush12345@users.noreply.github.com>
2024-07-12 23:09:25 +0530

    refactor: Update SignIn.js to improve validation and navigation

--- client/src/firebase/SignIn.js
+++ client/src/firebase/SignIn.js
@@ -1,23 +1,29 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from './firebase';
-import { signInSchema } from '../schemas/validationSchema';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./firebase";
+import { signInSchema } from "../schemas/validationSchema";
 
 const signInFn = async (email, password, navigate) => {
   try {
-
     const validation = signInSchema.safeParse({ email, password });
 
     if (!validation.success) {
-      return console.error(" Please enter valid information",validation.error.errors);
+      return console.error(
+        " Please enter valid information",
+        validation.error.errors
+      );
     }
 
-    const userCredential = await signInWithEmailAndPassword(auth, validation.data.email, validation.data.password);
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      validation.data.email,
+      validation.data.password
+    );
     const user = userCredential.user;
-    console.log('User signed in successfully:', user);
-    navigate('/account/dashboard');
+    console.log("User signed in successfully:", user);
+    navigate("/account/settings");
     return user;
   } catch (error) {
-    console.error('Error signing in:', error.message);
+    console.error("Error signing in:", error.message);
     throw error;
   }
 };
