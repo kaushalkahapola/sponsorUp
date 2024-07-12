d27cf828090c65a22101bf34c9ce6f32af48f8f6
anush12345 <75567234+anush12345@users.noreply.github.com>
2024-07-12 19:02:55 +0530

    chore: Refactor SignUp.js for improved readability and error handling

--- client/src/firebase/SignUp.js
+++ client/src/firebase/SignUp.js
@@ -1,31 +1,41 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth, db } from './firebase';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "./firebase";
 import { doc, setDoc } from "firebase/firestore";
-import { signUpSchema } from '../schemas/validationSchema';
+import { signUpSchema } from "../schemas/validationSchema";
 
 const signUpFn = async (email, password, confirmPassword, navigate) => {
   try {
-
-    const validation = signUpSchema.safeParse({ email, password, confirmPassword });
+    const validation = signUpSchema.safeParse({
+      email,
+      password,
+      confirmPassword,
+    });
     if (!validation.success) {
-    return console.error(" Please enter valid information",validation.error.errors);
-  }
+      return console.error(
+        " Please enter valid information",
+        validation.error.errors
+      );
+    }
 
-    const userCredential = await createUserWithEmailAndPassword(auth, validation.data.email, validation.data.password);
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      validation.data.email,
+      validation.data.password
+    );
     const user = userCredential.user;
-    
+
     const data = {
       email: user.email,
       uid: user.uid,
     };
-    
-    await setDoc(doc(db, "users", user.uid ), data); 
 
-    console.log('User signed up successfully:', user);
-    navigate('/account/dashboard');
+    await setDoc(doc(db, "users", user.uid), data);
+
+    console.log("User signed up successfully:", user);
+    navigate("/obq");
     return user;
   } catch (error) {
-    console.error('Error signing up:', error.message);
+    console.error("Error signing up:", error.message);
     throw error;
   }
 };
