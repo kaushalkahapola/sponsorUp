9c541ce362cbbcbcc62251ed34cf7f5604b05b0a
kaushalkahapola <kaushal.21@cse.mrt.ac.lk>
2024-07-12 12:27:37 +0530

    Update HomeHeader component to use "Sign in" instead of "Log in" for the button text

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,13 +1,19 @@
-import { EnvelopeClosedIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+import {
+  EnvelopeClosedIcon,
+  ArrowRightIcon,
+  CheckboxIcon,
+} from "@radix-ui/react-icons";
 import TBImage from "../assets/TB.png";
-import BAHImage from "../assets/BAH.png";
-import MPImage from "../assets/MP.png";
-import MCImage from "../assets/MC.png";
+import hero from "../assets/hero.svg";
+import whychoose from "../assets/whychoose.svg";
+import howwork from "../assets/howwork.svg";
 import OABImage from "../assets/OAB.png";
 import Footer from "../components/Footer";
-import { Container } from "@radix-ui/themes";
+import { Container, Heading, Text } from "@radix-ui/themes";
 import HomeHeader from "../components/HomeHeader";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../components/Buttons";
 
 const LandingPage = () => {
   const [email, setEmail] = useState("");
@@ -34,14 +40,18 @@ const LandingPage = () => {
                 <div className="order-2 md:order-1 md:px-0 text-center md:text-left">
                   <div className="">
                     <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold text-white leading-normal">
-                      Elevate Your Events <br className="hidden lg:block" /><span className="font-medium">with</span><br className="hidden lg:block"/>
-                      Seamless Sponsorship
-                      Connections
+                      Elevate Your Events <br className="hidden lg:block" />
+                      <span className="font-medium">with</span>
+                      <br className="hidden lg:block" />
+                      Seamless Sponsorship Connections
                     </h1>
                     <p className="mt-4 text-lg text-white">
                       Connecting you with the right sponsors to grow your brand.
                     </p>
-                    <form className="mt-6 flex justify-center md:justify-start" onSubmit={handleGetStarted}>
+                    <form
+                      className="mt-6 flex justify-center md:justify-start"
+                      onSubmit={handleGetStarted}
+                    >
                       <div className="relative w-full md:w-auto">
                         <div className="absolute inset-y-0 left-4 flex items-center pointer-events-none">
                           <EnvelopeClosedIcon className="w-5 h-5 text-gray-400" />
@@ -54,7 +64,10 @@ const LandingPage = () => {
                           onChange={(e) => setEmail(e.target.value)}
                           required
                         />
-                        <button type="submit" className="absolute right-1 top-1 bottom-1 bg-purple-500 text-gray-100 px-5 py-1.5 rounded-full m-1">
+                        <button
+                          type="submit"
+                          className="absolute right-1 top-1 bottom-1 bg-violet-500 text-white px-5 py-1.5 rounded-full m-1"
+                        >
                           Get Started
                         </button>
                       </div>
@@ -63,9 +76,9 @@ const LandingPage = () => {
                 </div>
                 <div className="order-2 flex items-center justify-center">
                   <img
-                    src={BAHImage}
+                    src={hero}
                     alt="Overlay Image"
-                    className="mx-auto max-w-80 md:max-w-full"
+                    className="mx-auto max-w-80 md:max-w-full mb-10"
                   />
                 </div>
               </div>
@@ -76,74 +89,133 @@ const LandingPage = () => {
         {/* Manage Customers */}
         <section className="relative bg-white py-10 md:py-20">
           <Container>
-          <div className="mx-auto px-5">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
-              <div className="order-2 md:order-2">
-                <img
-                  src={MCImage}
-                  alt="MC Image"
-                  className="mx-auto md:mr-12 md:ml-0 mb-8 md:mb-0 max-w-full"
-                />
-              </div>
-              <div className="order-1 md:order-1 text-center md:text-left justify-center space-y-1 md:space-y-3 lg:space-y-8">
-                <p className="text-gray-600">
-                  YOU CAN </p>
-                  <div className="font-semibold text-purple-700 text-3xl md:text-4xl lg:text-5xl">
-                    Monitor
-                  <span className="font-semibold text-black text-3xl md:text-4xl lg:text-5xl">
-                    {' '}Customers
-                  </span>
+            <div className="mx-auto px-5">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+                <div className="order-2 md:order-1">
+                  <img
+                    src={whychoose}
+                    alt="MC Image"
+                    className="mx-auto md:mr-12 md:ml-0 mb-8 md:mb-0 max-w-full"
+                  />
+                </div>
+                <div className="order-1 md:order-2 text-center md:text-left justify-center space-y-1 md:space-y-3 lg:space-y-8">
+                  {/* <p className="text-gray-600">YOU CAN </p> */}
+                  <div className="font-semibold text-violet-700 text-3xl md:text-4xl lg:text-5xl">
+                    Why Choose
+                    <span className="font-semibold text-black text-3xl md:text-4xl lg:text-5xl">
+                      {" "}
+                      SponsorUp?
+                    </span>
                   </div>
-                  <p>
-                  View real-time updates on successful payments, refunds, and other transaction-related activities
-                </p>
-                <a
-                  href="#"
-                  className="text-purple-700 inline-flex items-center"
-                >
-                  Learn more <ArrowRightIcon className="w-5 h-5" />
-                </a>
+                  <ul className="space-y-3">
+                    <li className="flex items-start">
+                      <div className="inline">
+                        <CheckboxIcon color="green" />
+                      </div>
+                      <p className="inline ml-2">
+                        <span className="font-semibold">
+                          Effortless Connections:
+                        </span>{" "}
+                        We simplify the process of finding the perfect sponsors
+                        for your events.
+                      </p>
+                    </li>
+                    <li className="flex items-start">
+                      <div className="inline">
+                        <CheckboxIcon color="green" />
+                      </div>
+                      <p className="inline ml-2">
+                        <span className="font-semibold">
+                          Comprehensive Support:
+                        </span>{" "}
+                        From initial contact to final agreements, we provide
+                        support every step of the way.
+                      </p>
+                    </li>
+                    <li className="flex items-start">
+                      <div className="inline">
+                        <CheckboxIcon color="green" />
+                      </div>
+                      <p className="inline ml-2">
+                        <span className="font-semibold">Tailored Matches:</span>{" "}
+                        Our platform uses advanced algorithms to match
+                        organizers with the most suitable sponsors.
+                      </p>
+                    </li>
+                  </ul>
+                  <Link
+                    to="/signup"
+                    className="text-violet-700 inline-flex items-center"
+                  >
+                    Get Started
+                    <ArrowRightIcon className="w-5 h-5 ml-1" />
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
           </Container>
         </section>
 
         {/* Manage Payments */}
         <section className="relative bg-white py-10 md:py-20">
           <Container>
-          <div className="mx-auto px-5">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
-              <div className="order-2 md:order-1">
-                <img
-                  src={MPImage}
-                  alt="MP Image"
-                  className="mx-auto max-w-full"
-                />
-              </div>
-              <div className="order-1 md:order-2 text-center md:text-left justify-center space-y-1 md:space-y-3 lg:space-y-8">
-                <p className="text-gray-600">
-                  YOU CAN </p>
-                  <div className="font-semibold text-purple-700 text-3xl md:text-4xl lg:text-5xl">
-                    Monitor
-                  <span className="font-semibold text-black text-3xl md:text-4xl lg:text-5xl">
-                    {' '}Payments
-                  </span>
+            <div className="mx-auto px-5">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+                <div className="order-2 md:order-2">
+                  <img
+                    src={howwork}
+                    alt="MP Image"
+                    className="mx-auto max-w-full"
+                  />
+                </div>
+                <div className="order-1 md:order-1 text-center md:text-left justify-center space-y-1 md:space-y-3 lg:space-y-8">
+                  {/* <p className="text-gray-600">YOU CAN </p> */}
+                  <div className="font-semibold text-violet-700 text-3xl md:text-4xl lg:text-5xl">
+                    How It
+                    <span className="font-semibold text-black text-3xl md:text-4xl lg:text-5xl">
+                      {" "}
+                      Works
+                    </span>
                   </div>
-                  <p>
-                  View real-time updates on successful payments, refunds,
-                  and other transaction-
-                  related activities.
-                </p>
-                <a
-                  href="#"
-                  className="text-purple-700 inline-flex items-center"
-                >
-                  Learn more <ArrowRightIcon className="w-5 h-5 ml-1" />
-                </a>
+                  <ul className="space-y-3">
+                    <li className="flex items-start">
+                      <div className="inline"></div>
+                      <p className="inline ml-2">
+                        <span className="font-semibold">Step 1: Sign Up:</span>{" "}
+                        Create an account as an event organizer or sponsor.
+                      </p>
+                    </li>
+                    <li className="flex items-start">
+                      <div className="inline"></div>
+                      <p className="inline ml-2">
+                        <span className="font-semibold">
+                          {" "}
+                          Step 2: Create/Explore Events:
+                        </span>{" "}
+                        Organizers can create detailed event profiles, and
+                        sponsors can explore events that match their interests.
+                      </p>
+                    </li>
+                    <li className="flex items-start">
+                      <div className="inline"></div>
+                      <p className="inline ml-2">
+                        <span className="font-semibold">
+                          Step 3: Connect & Collaborate:{" "}
+                        </span>
+                        Use our platform to reach out, propose sponsorships, and
+                        collaborate seamlessly.
+                      </p>
+                    </li>
+                  </ul>
+                  <Link
+                    to="/signup"
+                    className="text-violet-700 inline-flex items-center ml-2"
+                  >
+                    Sign up <ArrowRightIcon className="w-5 h-5 ml-1" />
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
           </Container>
         </section>
 
@@ -161,20 +233,16 @@ const LandingPage = () => {
         >
           <div className="absolute inset-0 bg-violet-200 opacity-50"></div>
           <Container>
-          <div className="relative z-10 text-white flex flex-col justify-center items-center h-full">
-            <div className="mx-auto text-center px-15">
-              <p className="text-black text-md md:text-lg lg:text-xl font-normal mb-6">
-                Simplify your business payments with our intuitive tool. Manage,
-                track, <br />
-                and optimize your financial transactions effortlessly. Take
-                control of your <br />
-                finances and focus on growing your business.
-              </p>
-              <button className="bg-purple-500 text-white px-6 py-3 rounded-lg hover:bg-purple-600 transition-colors duration-300">
-                Open Account
-              </button>
+            <div className="relative z-1 flex flex-col justify-center items-center h-full">
+              <div className="mx-auto text-center px-15 flex flex-col items-center space-y-5">
+                <Heading>Ready to Elevate Your Event?</Heading>
+                <Text>
+                  Join SponsorUp today and start connecting with the right
+                  sponsors.
+                </Text>
+                <Link to='/signup'><Button text="Sign Up Now" /> </Link>
+              </div>
             </div>
-          </div>
           </Container>
         </section>
 
